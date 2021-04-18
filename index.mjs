4ec62d743f6f2a86fa12cc0b5b64effa1ea4cb03
Minty <kinostl@gmail.com>
2021-04-18 14:30:36 -0400

    Numbers are easier than hexes

--- index.mjs
+++ index.mjs
@@ -11,18 +11,18 @@ const special_faces = ['highest number', 'lowest number', 'even number', 'odd nu
 const face_counts = [0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 14, 16, 18, 20, 24, 30, 34, 48, 50, 60, 100, 120]
 
 /** if anything rolls a second time, it uses the base, adds the first number of the hash, then uses that. If its the third time it uses the second integer. So on and so on. **/
-const dice_data = {}
+const diceData = {}
 const msg = 'Hello World'
-dice_data.hash = hasher.hash(Buffer.from(msg, 'utf8')).toString('hex')
-dice_data.lottery = dice_data.hash.substring(22, 24)
-// if(claimed(lottery)) return
-dice_data.face_count = dice_data.hash.substring(0, 2)
-dice_data.color = dice_data.hash.substring(2, 4)
-dice_data.modifier = dice_data.hash.substring(4, 6)
-dice_data.special_face = dice_data.hash.substring(6, 8)
-dice_data.emoji = dice_data.hash.substring(8, 11)
-dice_data.word = dice_data.hash.substring(11, 15)
-dice_data.phrase = dice_data.hash.substring(15, 18)
-dice_data.min = dice_data.hash.substring(18, 20)
-dice_data.max = dice_data.hash.substring(20, 22)
-console.table(dice_data)
+diceData.hash = hasher.hash(Buffer.from(msg, 'utf8')).toString('hex')
+diceData.lottery = parseInt(diceData.hash.substring(22, 24), 16)
+// if(claimed(lottery),16)) return
+diceData.face_count = parseInt(diceData.hash.substring(0, 2), 16)
+diceData.color = parseInt(diceData.hash.substring(2, 4), 16)
+diceData.modifier = parseInt(diceData.hash.substring(4, 6), 16)
+diceData.special_face = parseInt(diceData.hash.substring(6, 8), 16)
+diceData.emoji = parseInt(diceData.hash.substring(8, 11), 16)
+diceData.word = parseInt(diceData.hash.substring(11, 15), 16)
+diceData.phrase = parseInt(diceData.hash.substring(15, 18), 16)
+diceData.min = parseInt(diceData.hash.substring(18, 20), 16)
+diceData.max = parseInt(diceData.hash.substring(20, 22), 16)
+console.table(diceData)
