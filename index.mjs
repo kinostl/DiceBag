ad2327c011bd90d9fd4480d901020ecdc0dedb86
Minty <kinostl@gmail.com>
2021-04-22 05:38:30 -0400

    Moved into a more good formatting

--- index.mjs
+++ index.mjs
@@ -13,48 +13,50 @@ const diceSizes = ['5mm', '8mm', '12mm', '16mm', '19mm', '25mm', '50mm']
 
 const faceCounts = [0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 14, 16, 18, 20, 24, 30, 34, 48, 50, 60, 100, 120]
 
-const diceData = {}
-const msg = 'Hello World'
+function getDiceData (msg) {
+  const diceData = {}
+  diceData.hash = hasher.hash(Buffer.from(msg, 'utf8')).toString('hex')
 
-diceData.hash = hasher.hash(Buffer.from(msg, 'utf8')).toString('hex')
+  const indexes = {}
+  indexes.tiny = diceData.hash.match(/.{1,1}/g).map((curr) => parseInt(curr, 16)) // 16
+  indexes.small = diceData.hash.match(/.{1,2}/g).map((curr) => parseInt(curr, 16)) // 256
+  indexes.big = diceData.hash.match(/.{1,3}/g).map((curr) => parseInt(curr, 16)) // 4096
+  indexes.huge = diceData.hash.match(/.{1,4}/g).map((curr) => parseInt(curr, 16)) // 65536
+  indexes.get = (i, size = 'tiny') => {
+    if (size === 'huge') return indexes.huge[i] || indexes.get(i, 'big')
+    if (size === 'big') return indexes.big[i] || indexes.get(i, 'small')
+    if (size === 'small') return indexes.small[i] || indexes.get(i, 'tiny')
+    if (size === 'tiny') return indexes.tiny[i] || i
+  }
 
-const indexes = {}
-indexes.tiny = diceData.hash.match(/.{1,1}/g).map((curr) => parseInt(curr, 16)) // 16
-indexes.small = diceData.hash.match(/.{1,2}/g).map((curr) => parseInt(curr, 16)) // 256
-indexes.big = diceData.hash.match(/.{1,3}/g).map((curr) => parseInt(curr, 16)) // 4096
-indexes.huge = diceData.hash.match(/.{1,4}/g).map((curr) => parseInt(curr, 16)) // 65536
-indexes.get = (i, size = 'tiny') => {
-  if (size === 'huge') return indexes.huge[i] || indexes.get(i, 'big')
-  if (size === 'big') return indexes.big[i] || indexes.get(i, 'small')
-  if (size === 'small') return indexes.small[i] || indexes.get(i, 'tiny')
-  if (size === 'tiny') return indexes.tiny[i] || i
+  diceData.lottery = indexes.get(0, 'small')
+  // if(claimed(lottery)) return
+  diceData.faceCounts = faceCounts[indexes.get(1, 'small') % faceCounts.length]
+
+  diceData.size = indexes.get(0) === indexes.get(1) ? 'foam' : diceSizes[indexes.get(0)] || '16mm'
+  diceData.specialType = indexes.get(2) === indexes.get(3) ? 'slurry' : specialTypes[indexes.get(1)] || 'none'
+  diceData.gimmick = gimmicks[indexes.get(2)] || 'none'
+  diceData.material = materials[indexes.get(3)] || 'plastic'
+  diceData.color = colors[indexes.get(4) % colors.length]
+
+  // Step 8 Populate every face as described below.
+  diceData.faces = []
+  const faceCalcs = {}
+
+  faceCalcs.none = (i) => i + 1
+
+  faceCalcs.words = (i) => dic[indexes.get(i, 'huge') % dic.length]
+  faceCalcs.phrases = (i) => phrases[indexes.get(i, 'big') % phrases.length]
+  faceCalcs.fudge = (i) => ['-', ' ', '+'][i % 3]
+  faceCalcs.symbols = (i) => emojis[indexes.get(i, 'huge') % emojis.length]
+
+  faceCalcs.slurry = (i) => faceCalcs[specialTypes[i % specialTypes.length]](i)
+
+  for (let i = 0; i < diceData.faceCounts; i++) {
+    diceData.faces[i] = faceCalcs[diceData.specialType](i)
+  }
+  return diceData
 }
 
-diceData.lottery = indexes.get(0, 'small')
-// if(claimed(lottery)) return
-diceData.faceCounts = faceCounts[indexes.get(1, 'small') % faceCounts.length]
-
-diceData.size = indexes.get(0) === indexes.get(1) ? 'foam' : diceSizes[indexes.get(0)] || '16mm'
-diceData.specialType = indexes.get(2) === indexes.get(3) ? 'slurry' : specialTypes[indexes.get(1)] || 'none'
-diceData.gimmick = gimmicks[indexes.get(2)] || 'none'
-diceData.material = materials[indexes.get(3)] || 'plastic'
-diceData.color = colors[indexes.get(4) % colors.length]
-
-// Step 8 Populate every face as described below.
-diceData.faces = []
-const faceCalcs = {}
-
-faceCalcs.none = (i) => i + 1
-
-faceCalcs.words = (i) => dic[indexes.get(i, 'huge') % dic.length]
-faceCalcs.phrases = (i) => phrases[indexes.get(i, 'big') % phrases.length]
-faceCalcs.fudge = (i) => ['-', ' ', '+'][i % 3]
-faceCalcs.symbols = (i) => emojis[indexes.get(i, 'huge') % emojis.length]
-
-faceCalcs.slurry = (i) => faceCalcs[specialTypes[i % specialTypes.length]](i)
-
-for (let i = 0; i < diceData.faceCounts; i++) {
-  diceData.faces[i] = faceCalcs[diceData.specialType](i)
-}
-
-console.log(diceData)
+console.log('Hello World', getDiceData('Hello World'))
+console.log('Merry Mancer Games', getDiceData('Merry Mancer Games'))
