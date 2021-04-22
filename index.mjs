9c55f1368a546e0d0c540303a3cf4c3cf496792a
Minty <kinostl@gmail.com>
2021-04-22 05:32:47 -0400

    Algo complete

--- index.mjs
+++ index.mjs
@@ -3,12 +3,15 @@ import dic from './dictionary.js'
 import phrases from './phrases.js'
 import emojis from './emoji.js'
 
+console.log('dic', dic.length)
+console.log('phrases', phrases.length)
+console.log('emojies', emojis.length)
 const hasher = new XXHash128()
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'pink', 'purple', 'black', 'white', 'brown']
 const gimmicks = ['glows in the dark', 'glitters', 'object inside', 'round']
 const materials = ['metal', 'wood', 'glass']
-const specialTypes = ['words', 'phrases', 'fudge', 'symbols']
+const specialTypes = ['words', 'phrases', 'fudge', 'symbols', 'none']
 const diceSizes = ['5mm', '8mm', '12mm', '16mm', '19mm', '25mm', '50mm']
 
 const faceCounts = [0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 14, 16, 18, 20, 24, 30, 34, 48, 50, 60, 100, 120]
@@ -18,41 +21,43 @@ const msg = 'Hello World'
 
 diceData.hash = hasher.hash(Buffer.from(msg, 'utf8')).toString('hex')
 
-diceData.tinyIndexes = diceData.hash.match(/.{1,1}/g).map((curr) => parseInt(curr, 16)) // 16
-diceData.indexes = diceData.hash.match(/.{1,2}/g).map((curr) => parseInt(curr, 16)) // 256
-diceData.bigIndexes = diceData.hash.match(/.{1,3}/g).map((curr) => parseInt(curr, 16)) // 4096
-diceData.hugeIndexes = diceData.hash.match(/.{1,4}/g).map((curr) => parseInt(curr, 16)) // 65536
+const indexes = {}
+indexes.tiny = diceData.hash.match(/.{1,1}/g).map((curr) => parseInt(curr, 16)) // 16
+indexes.small = diceData.hash.match(/.{1,2}/g).map((curr) => parseInt(curr, 16)) // 256
+indexes.big = diceData.hash.match(/.{1,3}/g).map((curr) => parseInt(curr, 16)) // 4096
+indexes.huge = diceData.hash.match(/.{1,4}/g).map((curr) => parseInt(curr, 16)) // 65536
+indexes.get = (i, size = 'tiny') => {
+  if (size === 'huge') return indexes.huge[i] || indexes.get(i, 'big')
+  if (size === 'big') return indexes.big[i] || indexes.get(i, 'small')
+  if (size === 'small') return indexes.small[i] || indexes.get(i, 'tiny')
+  if (size === 'tiny') return indexes.tiny[i] || i
+}
 
-diceData.lottery = diceData.indexes[0]
+diceData.lottery = indexes.get(0, 'small')
 // if(claimed(lottery)) return
-diceData.faceCounts = faceCounts[diceData.indexes[1] % faceCounts.length]
+diceData.faceCounts = faceCounts[indexes.get(1, 'small') % faceCounts.length]
 
-diceData.size = diceData.tinyIndexes[0] === diceData.tinyIndexes[1] ? 'foam' : diceSizes[diceData.tinyIndexes[0]] || '16mm'
-diceData.specialType = diceData.tinyIndexes[2] === diceData.tinyIndexes[3] ? 'slurry' : specialTypes[diceData.tinyIndexes[1]] || 'none'
-diceData.gimmick = gimmicks[diceData.tinyIndexes[2]] || 'none'
-diceData.material = materials[diceData.tinyIndexes[3]] || 'plastic'
-diceData.color = colors[diceData.tinyIndexes[4] % colors.length]
+diceData.size = indexes.get(0) === indexes.get(1) ? 'foam' : diceSizes[indexes.get(0)] || '16mm'
+diceData.specialType = indexes.get(2) === indexes.get(3) ? 'slurry' : specialTypes[indexes.get(1)] || 'none'
+diceData.gimmick = gimmicks[indexes.get(2)] || 'none'
+diceData.material = materials[indexes.get(3)] || 'plastic'
+diceData.color = colors[indexes.get(4) % colors.length]
 
 // Step 8 Populate every face as described below.
 diceData.faces = []
 const faceCalcs = {}
+
 faceCalcs.none = (i) => i + 1
 
-faceCalcs.words = (i) => dic[i] || i + 1
-faceCalcs.phrases = (i) => phrases[i] || i + 1
+faceCalcs.words = (i) => dic[indexes.get(i, 'huge') % dic.length]
+faceCalcs.phrases = (i) => phrases[indexes.get(i, 'big') % phrases.length]
 faceCalcs.fudge = (i) => ['-', ' ', '+'][i % 3]
-faceCalcs.symbols = (i) => emojis[i] || i + 1
+faceCalcs.symbols = (i) => emojis[indexes.get(i, 'huge') % emojis.length]
 
 faceCalcs.slurry = (i) => faceCalcs[specialTypes[i % specialTypes.length]](i)
 
 for (let i = 0; i < diceData.faceCounts; i++) {
   diceData.faces[i] = faceCalcs[diceData.specialType](i)
 }
-
-/* When a dice is a specialType every face has that specialType. The hex value will be iterated over to acquire indexes. If an index does not return a value, the index is used as the face instead - producing a number. */
-
-/* When you totally run out of indexes, return an increasing number */
-
-/* When you run out of indexes in a list of indexes, fall back to the smaller one. If you're already a tinyIndex, go up in size and use modulo to bring yourself back down to the proper limit. */
 
 console.log(diceData)
