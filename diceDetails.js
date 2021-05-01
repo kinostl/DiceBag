72599fe4a3e2035f66f991c4d37afea2239a32e3
Minty <kinostl@gmail.com>
2021-05-01 05:49:03 -0400

    Moving the big stuff into its own files

---
+++ diceDetails.js
@@ -0,0 +1,55 @@
+module.exports = {}
+module.exports.colors = [
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'pink',
+  'purple',
+  'black',
+  'white',
+  'brown'
+]
+module.exports.gimmicks = [
+  'glows in the dark',
+  'glitters',
+  'object inside',
+  'round'
+]
+module.exports.materials = ['metal', 'wood', 'glass']
+module.exports.specialTypes = ['words', 'phrases', 'fudge', 'symbols', 'pipped']
+module.exports.diceSizes = [
+  '5mm',
+  '8mm',
+  '12mm',
+  '16mm',
+  '19mm',
+  '25mm',
+  '50mm'
+]
+module.exports.faceCounts = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  10,
+  12,
+  14,
+  16,
+  18,
+  20,
+  24,
+  30,
+  34,
+  48,
+  50,
+  60,
+  100,
+  120
+]
